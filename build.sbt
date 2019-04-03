7baf67c088d76fdd94bc5bb8245c7e9bc8d7275c
root <root@iZ2ze3pjjhxv6g0twxdd7dZ.(none)>
2019-04-03 11:34:15 +0800

    ... ...

---
+++ build.sbt
@@ -0,0 +1,4 @@
+name:="MetisByScala"
+organization:="cn.sh.corpize"
+version:="0.1-SNAPSHOT"
+scalaVersion:="2.11.8"
---
+++ gitProjScala.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $# -gt 1 ]
+then
+  send_user "Error, no argument , usage: expect  crtMvnProj.exp projName\n"
+  exit
+else
+  cd $1
+  touch build.properties
+  echo "sbt.version=1.2.8" > build.properties
+  touch build.sbt
+  echo "name:=\"$1\"" > build.sbt
+  echo "organization:=\"cn.sh.corpize\"" >> build.sbt
+  echo "version:=\"0.1-SNAPSHOT\"" >> build.sbt
+  echo "scalaVersion:=\"2.11.8\"" >> build.sbt
+
+fi
